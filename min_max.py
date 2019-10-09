260dc091cbf476a90cf7704d6c9e4aa643439fa9
Mattie Zilaie <your_name@domain.comAccount>
2019-10-08 17:51:54 -0700

    asks the user how many integers they would like to enter then prompt the user to enter that many integers  then display the largest and smallest of those numbers

--- min_max.py
+++ min_max.py
@@ -9,14 +9,17 @@ max = 0
 user_value = int(input('How many integers would you like to enter?'))
 print(user_value)
 print("Please enter", user_value, "integers.")
-min = user_value
-max = user_value
+
 for len in range(user_value):
     numbs =int(input())
-    print(numbs)
-    if (numbs<min):
+
+    if len ==0:
+        max = numbs
         min = numbs
-    if (numbs>max):
-        max=numbs
+
+if (numbs<min):
+    min = numbs
+elif (numbs>max):
+    max=numbs
 print("min:", min)
 print("max:", max)
